6bdd09dbdf8582ffe0f0f951c9520c3d875d4b9b
Niclas Spas <niclasspas@gmail.com>
2021-04-09 11:20:11 +0200

    work work

--- frontend/pnp-frontend/src/app/app.module.ts
+++ frontend/pnp-frontend/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule,
+    CommonModule,
+    FlexLayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- frontend/pnp-frontend/src/app/auth/register/register.component.ts
+++ frontend/pnp-frontend/src/app/auth/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,11 +14,75 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  constructor(private router: Router) {}
+  public registerForm: FormGroup;
+
+  constructor(private router: Router, private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group(
+      {
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              '^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,}$'
+            ),
+          ],
+        ],
+        confirmPassword: ['', Validators.required],
+      },
+      this.passwordMatchValidator
+    );
+  }
 
   ngOnInit(): void {}
 
   public redirectLogin() {
-    this.router.n('auth/login');
+    this.router.navigate(['login']);
+  }
+
+  public passwordMatchValidator(
+    control: AbstractControl
+  ): ValidationErrors | null {
+    const password: string = control.get('password').value;
+    const confirmPassword: string = control.get('confirmPassword').value;
+    console.log('pw: ', password, ' cpw: ', confirmPassword);
+
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  public register() {
+    console.log(' This', this.registerForm.controls.password.errors);
+  }
+
+  public getEmailErrorMessage() {
+    if (this.registerForm.controls.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.registerForm.controls.email.hasError('email')
+      ? 'Not a valid email'
+      : '';
+  }
+
+  public getPasswordErrorMessage() {
+    if (this.registerForm.controls.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.registerForm.controls.email.hasError('pattern')
+      ? 'Password must be a minimum of 8 characters. It must also contain at least 1 lower and 1 upper case letter, a number and a special character: $ ! % * ? &'
+      : '';
+  }
+
+  public getConfirmPasswordErrorMessage() {
+    if (this.registerForm.controls.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.registerForm.hasError('passwordMismatch')
+      ? 'Must match password'
+      : 'humbug';
   }
 }
