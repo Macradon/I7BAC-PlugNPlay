efacd43f7830a07d2bb21e7d80067d0acdb2e608
Macradon <36033419+Macradon@users.noreply.github.com>
2021-05-19 12:35:31 +0200

    Added Source Bundle for publishing

--- PlugNPlayBackend/Program.cs
+++ PlugNPlayBackend/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -41,7 +42,10 @@ namespace PlugNPlayBackend
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseKestrel()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseIISIntegration()
+                    .UseStartup<Startup>();
                 });
     }
 }
